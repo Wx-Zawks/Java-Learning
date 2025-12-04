92d269842393475886f2849a9e637e9bd5cbd754
zhouzhou <329203@whut.edu.cn>
2025-12-04 11:27:52 +0800

    laptop:阿里云oss样例配置

---
+++ DeptManageSystem/src/main/java/demo/zhouzhou/DeptManageSystemApplication.java
@@ -0,0 +1,13 @@
+package demo.zhouzhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptManageSystemApplication.class, args);
+    }
+
+}
---
+++ DeptManageSystem/src/test/java/demo/zhouzhou/DeptManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.zhouzhou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeptManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
