7131bafa57256ca2b5913ef8bbaec8abecfa4d15
zhouzhou <329203@whut.edu.cn>
2025-12-04 15:34:54 +0800

    laptop:上传文件集成OSS(链接未保存数据库)

--- DeptManageSystem/src/main/java/demo/zhouzhou/controller/UploadController.java
+++ DeptManageSystem/src/main/java/demo/zhouzhou/controller/UploadController.java
@@ -1,7 +1,9 @@
 package demo.zhouzhou.controller;
 
 import demo.zhouzhou.pojo.Result;
+import demo.zhouzhou.utils.AliyunOSSOperator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,13 +15,24 @@ import java.util.UUID;
 @Slf4j
 @RestController
 public class UploadController {
+    //本地上传文件
+//    @PostMapping("/upload")
+//    public Result upload(String name, Integer age, MultipartFile file) throws IOException {
+//        log.info("上传文件: {}, {}, {}, {}", name, age, file.getOriginalFilename(), file.getSize());
+//        String originalFilename = file.getOriginalFilename();
+//        String newFilename = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
+//        file.transferTo(new File("E:\\JavaProject\\JavaWeb\\DeptManageStystem\\DeptManageSystem\\src\\main\\resources\\" + newFilename));
+//        return Result.success();
+//    }
+    @Autowired
+    private AliyunOSSOperator aliyunOSSOperator;
 
     @PostMapping("/upload")
-    public Result upload(String name, Integer age, MultipartFile file) throws IOException {
-        log.info("上传文件: {}, {}, {}, {}", name, age, file.getOriginalFilename(), file.getSize());
-        String originalFilename = file.getOriginalFilename();
-        String newFilename = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
-        file.transferTo(new File("E:\\JavaProject\\JavaWeb\\DeptManageStystem\\DeptManageSystem\\src\\main\\resources\\" + newFilename));
-        return Result.success();
+    public Result upload(MultipartFile file) throws Exception {
+        log.info("文件上传: {}", file.getOriginalFilename() );
+        //文件交给OSS操作管理
+        String url = aliyunOSSOperator.upload(file.getBytes(),file.getOriginalFilename());
+        log.info("文件上传到OSS的URL: {}", url);
+        return Result.success(url);
     }
 }
