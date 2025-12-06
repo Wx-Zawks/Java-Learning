dca37b15cd5e841ddbc0a8c4680ecce4ffbd537a
wxz <1285623719@qq.com>
2025-12-06 16:45:06 +0800

    阿里云OSS优化(未将url保存至数据库)

--- DeptManageSystem/src/main/java/demo/zhouzhou/controller/UploadController.java
+++ DeptManageSystem/src/main/java/demo/zhouzhou/controller/UploadController.java
@@ -1,7 +1,10 @@
 package demo.zhouzhou.controller;
 
 import demo.zhouzhou.pojo.Result;
+import demo.zhouzhou.utils.AliyunOSSOperator;
+import demo.zhouzhou.utils.AliyunOSSProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,12 +17,20 @@ import java.util.UUID;
 @RestController
 public class UploadController {
 
+    @Autowired
+    private AliyunOSSOperator aliyunOSSOperator;
+
     @PostMapping("/upload")
-    public Result upload(String name, Integer age, MultipartFile file) throws IOException {
-        log.info("上传文件: {}, {}, {}, {}", name, age, file.getOriginalFilename(), file.getSize());
-        String originalFilename = file.getOriginalFilename();
-        String newFilename = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
-        file.transferTo(new File("E:\\JavaProject\\JavaWeb\\DeptManageStystem\\DeptManageSystem\\src\\main\\resources\\" + newFilename));
-        return Result.success();
+    public Result upload(MultipartFile file) throws Exception {
+//        log.info("上传文件: {}, {}, {}, {}", name, age, file.getOriginalFilename(), file.getSize());
+//        String originalFilename = file.getOriginalFilename();
+//        String newFilename = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
+//        file.transferTo(new File("E:\\JavaProject\\JavaWeb\\DeptManageStystem\\DeptManageSystem\\src\\main\\resources\\" + newFilename));
+//        return Result.success();
+
+        //阿里云OSS上传文件
+        log.info("阿里云OSS上传文件:{}",file.getOriginalFilename());
+        String url = aliyunOSSOperator.upload(file.getBytes(), file.getOriginalFilename());
+        return Result.success(url);
     }
 }
